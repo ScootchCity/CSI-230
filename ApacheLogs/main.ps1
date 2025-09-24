ff3df6037a4054d0f982846a8435a4a74da11747
ScootchCity <lloyd.ivester@mymail.champlain.edu>
2025-09-24 01:45:30 -0400

    did parsing apache logs

--- ApacheLogs/main.ps1
+++ ApacheLogs/main.ps1
@@ -1,5 +1,10 @@
 ﻿. .\Apache-Logs.ps1
+. .\parsingLogs.ps1
 
 $ips = Get-Apache-Logs -pageRef "index.html" -httpCode "200" -browser "chrome"
 
 $ips
+
+$tableRecords = ApacheLogs1
+
+$tableRecords | Format-table -AutoSize -Wrap
---
+++ ApacheLogs/parsingLogs.ps1
@@ -0,0 +1,20 @@
+﻿function ApacheLogs1(){
+$logsnotformatted = Get-Content C:\xampp\apache\logs\access.log
+$tableRecords = @()
+
+for($i = 0; $i -lt $logsnotformatted.Count; $i++){
+    #Split a string into words
+    $words = $logsnotformatted[$i].Split(" ");
+
+    $tableRecords += [PSCustomObject]@{ "IP" = $words[0];
+                                        "Time" = $words[3];#.Trim('['); had to remove this as it was giving me formatting problems that i cant figure out
+                                        "Method" = $words[5].Trim('"');
+                                        "Page" = $words[6];
+                                        "Protocol" = $words[7];
+                                        "Response" = $words[8];
+                                        "Referrer" = $words[10];
+                                        "Client" = $words[11..($words.Length-1)];
+    }
+}
+return $tableRecords | Where-Object { $_.IP -ilike "10.*" }
+}
