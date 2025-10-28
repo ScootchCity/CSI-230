ea7194e16a8a844d2c362e2bd554b0ca822ebde5
ScootchCity <lloyd.ivester@mymail.champlain.edu>
2025-10-28 17:20:26 -0400

    IPListing.bash

--- bash/ip_listing_address/IPList.bash
+++ bash/ip_listing_address/IPList.bash
@@ -9,7 +9,14 @@
 # Prefix is the first imput taken
 prefix=$1
 
+# Verify input length
+[ ${#prefix} -lt 5 ] && \
+printf "Prefix length is too short\nPrefix example: 10.0.17\n" &&\
+exit 1
+
 for i in {1..254}
 do
-    echo $prefix.$i
+    #using -q for a simple yes/no. We already know what ip is being
+    #pinged so it seems cleaner to me?
+    ping -c 1 $prefix.$i | grep -q '1 received' && echo $prefix.$i
 done
